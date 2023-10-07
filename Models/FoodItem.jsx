43193e56db95148bae59788e5f6b640e4484cbfe
VikrantPulipati <vikrant.pulipati@gmail.com>
2023-10-07 16:59:07 -0400

    Added FoodItem and Meal models, and started Generated Meals screen

---
+++ Models/FoodItem.jsx
@@ -0,0 +1,15 @@
+class FoodItem {
+    constructor (name, calories, carbs, fat, protein, sugar, sodium, cholesterol, fiber) {
+        this.name = name
+        this.calories = calories
+        this.carbs = carbs
+        this.fat = fat
+        this.protein = protein
+        this.sugar = sugar
+        this.sodium = sodium
+        this.cholesterol = cholesterol
+        this.fiber = fiber
+    }
+
+
+}
